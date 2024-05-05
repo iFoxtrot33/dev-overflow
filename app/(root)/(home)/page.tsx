91b6c5fc5f45fa3df759b1727365e78a54f90490
Iurii Furman <furmaniurii@gmail.com>
2024-05-05 13:54:57 +0400

    feat: set up theme

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
-const Home = () => {
-  return (
-    <div>
-      Hi
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <UserButton afterSignOutUrl="/" />
+  </div>
+);
 
 export default Home;
